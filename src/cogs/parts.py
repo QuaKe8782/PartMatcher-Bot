d2e6480cd7c0b758dda7b1b95c601badd911c16b
QuaKe8782 <mastermind4560@gmail.com>
2021-04-02 10:52:28 +0100

    Add descriptions to commands in command group

--- src/cogs/parts.py
+++ src/cogs/parts.py
@@ -7,12 +7,13 @@ class Parts(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(invoke_without_command=True, aliases=["pm"])
+
+    @commands.group(invoke_without_command=True, aliases=["pm"], description="Lists all PartMatcher commands.")
     async def partmatcher(self, ctx):
-        await ctx.send("test")
+        pass
 
 
-    @partmatcher.command()
+    @partmatcher.command(description="Submit a part for verification.")
     async def submit(self, ctx):
         await ctx.send("submit")
 
