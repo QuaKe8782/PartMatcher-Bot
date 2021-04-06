4651551cbedde73cc76df0921e15ae340b2f4e73
QuaKe8782 <mastermind4560@gmail.com>
2021-04-06 22:01:22 +0100

    Add Exceptions

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,15 @@
 import discord
 
+
 class Embed(discord.Embed):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.colour = discord.Colour(0x14d18c)
+
+
+class UserCancel(Exception):
+    pass
+
+
+class MessageTimeout(Exception):
+    pass
