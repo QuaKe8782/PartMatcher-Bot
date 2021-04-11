ff4df61d5001fc59543987cb38cb69fdd092cc9d
QuaKe8782 <mastermind4560@gmail.com>
2021-04-11 21:36:03 +0100

    Add non case-sensitive member converter

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 
 
 class Embed(discord.Embed):
@@ -14,3 +15,28 @@ class UserCancel(Exception):
 
 class MessageTimeout(Exception):
     pass
+
+
+def get_member(guild, **attrs):
+    name = attrs["name"]
+
+    try:
+        discriminator = attrs["discriminator"]
+    except KeyError:
+        discriminator = None
+
+    user = f"{name}#{discriminator}" if discriminator else name
+
+    for member in guild.members:
+        if user.lower() in str(member).lower():
+            return member
+
+
+class Member(commands.MemberConverter):
+    async def query_member_named(self, guild, argument):
+        if len(argument) > 5 and argument[-5] == '#':
+            username, _, discriminator = argument.rpartition('#')
+
+            return get_member(guild, name=username, discriminator=discriminator)
+        else:
+            return get_member(guild, name=argument)
