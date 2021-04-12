66fe247d124f498711b0a29c00d728f5f84de4d6
QuaKe8782 <mastermind4560@gmail.com>
2021-04-12 08:41:05 +0100

    cogs/moderation: Add system to create list of embeds for warns

--- src/cogs/moderation.py
+++ src/cogs/moderation.py
@@ -10,6 +10,7 @@ import os
 from uuid import uuid1
 from utils import Member
 from datetime import datetime
+from math import ceil
 
 
 chars = list(ascii_lowercase + digits)
@@ -20,10 +21,8 @@ class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
     def gen_id(self, length):
         return ''.join([choice(chars) for i in range(length)])
-
 
     def generate_captcha(self):
         id = self.gen_id(6)
@@ -32,36 +31,35 @@ class Moderation(commands.Cog):
         image.write(id, path)
         return path, id
 
-
     @commands.Cog.listener()
     async def on_member_join(self, member):
         if member.guild.id != self.bot.pm_discord["pm_server"]:
             return
-        
+
         incorrect_attempts = 0
         while True:
             with ThreadPoolExecutor() as pool:
                 path, id = await asyncio.get_event_loop().run_in_executor(pool, self.generate_captcha)
-            
+
             file = discord.File(path, filename=f"{id}.png")
 
             embed = Embed(
-                title = "Verification",
-                description = "Please complete this CAPTCHA to continue."
+                title="Verification",
+                description="Please complete this CAPTCHA to continue."
             )
             embed.set_image(url=f"attachment://{os.path.basename(path)}.png")
             await member.send(file=file, embed=embed)
             os.remove(path)
 
-            check = lambda m: not m.guild and m.author == member
+            def check(m): return not m.guild and m.author == member
 
             try:
                 message = await self.bot.wait_for("message", check=check, timeout=60)
             except asyncio.TimeoutError:
                 embed = Embed(
-                    title = "You've didn't respond in time!",
-                    description = "You have been kicked from the Discord server. Please rejoin the server if this is a mistake.",
-                    colour = discord.Colour.red()
+                    title="You've didn't respond in time!",
+                    description="You have been kicked from the Discord server. Please rejoin the server if this is a mistake.",
+                    colour=discord.Colour.red()
                 )
                 await member.send(embed=embed)
                 await member.kick(reason="Failed CAPTCHA")
@@ -72,17 +70,17 @@ class Moderation(commands.Cog):
             else:
                 incorrect_attempts += 1
                 embed = Embed(
-                    title = "CAPTCHA failed",
-                    description = f"You have {3 - incorrect_attempts} attempts left.",
-                    colour = discord.Colour.red()
+                    title="CAPTCHA failed",
+                    description=f"You have {3 - incorrect_attempts} attempts left.",
+                    colour=discord.Colour.red()
                 )
                 await message.reply(embed=embed)
-            
+
             if incorrect_attempts >= 3:
                 embed = Embed(
-                    title = "You've run out of CAPTCHA attempts!",
-                    description = "You have been kicked from the Discord server. Please rejoin the server if this is a mistake.",
-                    colour = discord.Colour.red()
+                    title="You've run out of CAPTCHA attempts!",
+                    description="You have been kicked from the Discord server. Please rejoin the server if this is a mistake.",
+                    colour=discord.Colour.red()
                 )
                 await member.send(embed=embed)
                 await member.kick(reason="Failed CAPTCHA")
@@ -92,19 +90,18 @@ class Moderation(commands.Cog):
         await member.add_roles(member_role, reason="Completed CAPTCHA")
 
         embed = Embed(
-            title = "Success! You have completed the CAPTCHA!",
-            description = "You have gained access to the rest of the server."
+            title="Success! You have completed the CAPTCHA!",
+            description="You have gained access to the rest of the server."
         )
         await message.reply(embed=embed)
-
 
     @commands.has_guild_permissions(kick_members=True)
     @commands.command()
     async def warn(self, ctx, member: Member = None, reason="No reason provided."):
         if not member:
             embed = Embed(
-                title = "You need to tell me who to warn!",
-                colour = discord.Colour.red()
+                title="You need to tell me who to warn!",
+                colour=discord.Colour.red()
             )
             await ctx.reply(embed=embed)
 
@@ -119,34 +116,53 @@ class Moderation(commands.Cog):
         await self.bot.db["DiscordBot"]["Warns"].insert_one(warn_object)
 
         embed = Embed(
-            title = f"Warned {member} for:",
-            description = reason
+            title=f"Warned {member} for:",
+            description=reason
         )
 
         await ctx.reply(embed=embed)
 
         embed = Embed(
-            title = "You have been warned in PartMatcher for:",
-            description = reason,
-            colour = discord.Colour.red()
+            title="You have been warned in PartMatcher for:",
+            description=reason,
+            colour=discord.Colour.red()
         )
 
         await member.send(embed=embed)
 
-
     @commands.command(aliases=["warnings", "getwarns", "showwarns"])
     async def warns(self, ctx, member: Member):
         warns = await self.bot.db["DiscordBot"]["Warns"].find({"user": member.id}).to_list(length=50)
-        
+
         if not warns:
             embed = Embed(
-                title = f"{member} has no warns saved."
+                title=f"{member} has no warns saved."
             )
             await ctx.reply(embed=embed)
             return
 
-        
+        embeds = []
 
+        for i in range(ceil(len(warns)/5)):
+            section_warns = warns[i * 5: i * 5 + 5]
+            embed = Embed(
+                title=f"Showing warns {i * 5 + 1} - {i * 5 + len(section_warns)} of {len(warns)} for {member}:"
+            )
+
+            for warn in section_warns:
+                embed.add_field(
+                    name=f"Case ID: {warn['_id']}",
+                    value=f"""\
+Reason: {warn["reason"]}
+Moderator: <@{warn["mod"]}> ({warn["mod"]})
+Time: {warn["time"].strftime("%c")}
+"""
+                )
+
+            embeds.append(embed)
+
+            for embed in embeds:
+                await ctx.reply(embed=embed)
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
